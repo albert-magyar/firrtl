90944fd483f619c190934a9cf9e274451a4baedd
Albert Magyar <albert.magyar@gmail.com>
2018-11-08 18:07:32 -0800

    Add ExtractModel transform

---
+++ src/main/scala/firrtl/transforms/fame/ExtractModel.scala
@@ -0,0 +1,42 @@
+// See LICENSE for license details.
+
+package firrtl.transforms.fame
+
+import java.io.{PrintWriter, File}
+
+import firrtl._
+import ir._
+import Mappers._
+import Utils._
+import passes.MemPortUtils
+import transforms.{PromoteSubmodule, PromoteSubmoduleAnnotation}
+import annotations.{InstanceTarget, Annotation, SingleTargetAnnotation}
+
+import scala.collection.mutable
+import mutable.{LinkedHashSet, LinkedHashMap}
+
+case class FAMEModelAnnotation(target: InstanceTarget) extends SingleTargetAnnotation[InstanceTarget] {
+  def targets = Seq(target)
+  def duplicate(n: InstanceTarget) = this.copy(n)
+}
+
+class ExtractModel extends Transform {
+  def inputForm = HighForm
+  def outputForm = HighForm
+
+  def promoteModels(state: CircuitState): CircuitState = {
+    val anns = state.annotations.flatMap {
+      case a @ FAMEModelAnnotation(it) if (it.module != it.circuit) => Seq(a, PromoteSubmoduleAnnotation(it))
+      case a => Seq(a)
+    }
+    if (anns.toSeq == state.annotations.toSeq) {
+      state
+    } else {
+      promoteModels((new PromoteSubmodule).runTransform(state.copy(annotations = anns)))
+    }
+  }
+
+  override def execute(state: CircuitState): CircuitState = {
+    promoteModels(state)
+  }
+}
